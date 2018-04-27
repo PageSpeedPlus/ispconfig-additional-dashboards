1b3a0220cf57c5f75074e9f1f2976ee4989d7a20
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-04-27 11:19:18 +0200

    Add files via upload

---
+++ webftp/version.js
@@ -0,0 +1,7 @@
+var latest_stable_build   = 48;
+var latest_stable_version = '1.1';
+var latest_stable_url     = 'http://www.net2ftp.com/download/net2ftp_v1.1.zip';
+
+var latest_beta_build     = 48;
+var latest_beta_version   = '1.1';
+var latest_beta_url       = 'http://www.net2ftp.com/download/net2ftp_v1.1.zip';
