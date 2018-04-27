b428295eba41bc7f69edcfcfdb6e0fe591775228
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-04-27 11:28:32 +0200

    Add files via upload

---
+++ webftp/skins/shinra/google_ad_top.template.php
@@ -0,0 +1,3 @@
+<?php defined("NET2FTP") or die("Direct access to this location is not allowed."); ?>
+<!-- Template /skins/shinra/google_ad_top.template.php begin -->
+<!-- Template /skins/shinra/google_ad_top.template.php end -->
