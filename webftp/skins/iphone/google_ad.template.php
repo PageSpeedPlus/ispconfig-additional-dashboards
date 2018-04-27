5d4ff5eb492c3ba01170bdb90646cec94a21949a
Daniel Bieli <22905324+danielbieli@users.noreply.github.com>
2018-04-27 11:25:02 +0200

    Add files via upload

---
+++ webftp/skins/iphone/google_ad.template.php
@@ -0,0 +1,3 @@
+<?php defined("NET2FTP") or die("Direct access to this location is not allowed."); ?>
+<!-- Template /skins/iphone/google_ad.template.php begin -->
+<!-- Template /skins/iphone/google_ad.template.php end -->
